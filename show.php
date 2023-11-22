a27c4ce1121cdc3b276f0440a6ec70117ddaaf56
Wyine <148890732+Stellaro1@users.noreply.github.com>
2023-11-23 01:40:59 +0630

    Add files via upload

--- show.php
+++ show.php
@@ -4,6 +4,19 @@ $connection = mysqli_connect('localhost','root','','card');
 
 
 ?>
+<?php
+$result = [];
+
+function getResult($val)
+{
+  $connection = mysqli_connect('localhost', 'root', '', 'card');
+  $query = "SELECT * FROM card";
+  return $val = mysqli_query($connection, $query);
+}
+
+$result = getResult($result)
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -14,21 +27,23 @@ $connection = mysqli_connect('localhost','root','','card');
     <script src="https://cdn.tailwindcss.com"></script>
 
 </head>
-<body>
+<body class=" container ml-10">
     
-  <div>
+  
   <div class="w-full text-center mt-4">
-    <table class="w-full table-auto">
-      <thead class="border-b-2 border-gray-400 bg-gray-200">
+    <table class="shadow-purpl-800 w-full table-auto">
+      <thead class="border-b-2 border-gray-400 bg-purple-200">
         <tr>
-        <th class="font-serif text-sm">ID</th>
+        <th class="border border-slate-700 p-2   font-serif text-sm">ID</th>
          
-          <th class=" border border-slate-700 p-2 font-serif text-sm">Qusetion</th>
-          <th class=" border border-slate-700 p-2 font-serif text-sm">Answer1</th>
-          <th class=" border border-slate-700 font-serif text-sm">Answer2</th>
-          <th class=" border border-slate-700 p-2 font-serif text-sm">Answer3</th>
-          <th class=" border border-slate-700 p-2 font-serif text-sm">Answer4</th>
-          
+          <th class=" border border-slate-700 p-2 font-serif text-sm">‌မေးခွန်း</th>
+          <th class=" border border-slate-700 p-2 font-serif text-sm">‌အဖြေ ၁</th>
+          <th class=" border border-slate-700 font-serif text-sm">‌အဖြေ ၂</th>
+          <th class=" border border-slate-700 p-2 font-serif text-sm">‌အဖြေ ၃</th>
+          <th class=" border border-slate-700 p-2 font-serif text-sm">‌အဖြေ ၄</th>
+          <th class=" border border-slate-700 p-2 font-serif text-sm">အသစ်</th>
+          <th class=" border border-slate-700 p-2 font-serif text-sm">ဖြတ်ရန်</th>
+          <th class=" border border-slate-700 p-2 font-serif text-sm">ပြင်ဆင်ရန်</th>
         </tr>
       </thead>
       <?php
@@ -48,10 +63,10 @@ $connection = mysqli_connect('localhost','root','','card');
             echo "
             <td class='border border-slate-700'>$row[a3]</td>";
             echo "
-            <td class='border border-slate-700' >$row[a4]</td></tbody>";
-            
-            
-          
+            <td class='border border-slate-700' >$row[a4]</td>";
+            echo"<td class='border py-2 border-slate-700'><a href='insert.php' class='px-3 bg-yellow-100 hover:bg-yellow-300 rounded text-xs p-1 font-serif'> New</a></td>";
+            echo"<td class='border border-slate-700'><a href='show.php?id_to_delete=$row[id]'  class='px-3 text-white hover:bg-red-800 bg-red-600 rounded text-xs p-1 font-serif'> Delete</a></td>";
+            echo"<td class='border border-slate-700' ><a href='insert.php?id_to_update=$row[id]' class='px-3 bg-purple-200 hover:bg-purple-500 rounded text-xs p-1 font-serif'>Update</a></td>";
           }
          }
        ?>
@@ -60,3 +75,18 @@ $connection = mysqli_connect('localhost','root','','card');
   </div><!--showdata-->
 </body>
 </html>
+<?php
+if (isset($_GET['id_to_delete'])) {
+  $connection = mysqli_connect('localhost','root','','card');
+
+  $delete_id = $_GET['id_to_delete'];
+  $ok = "DELETE FROM card WHERE id=$delete_id";
+  $resude = mysqli_query($connection, $ok);
+  if ($resude) {
+    $result=getResult($resude);
+    echo "<script> alert('Successfully Deleted');</script>";
+  } else {
+    die('Error' . mysqli_error($connection));
+  }}
+  ?>
+ 
