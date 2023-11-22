c86b0969ccbd030f0eaf1805436c6f191d4b625d
Wyine <148890732+Stellaro1@users.noreply.github.com>
2023-11-23 01:40:40 +0630

    Add files via upload

--- insert.php
+++ insert.php
@@ -15,7 +15,7 @@ if(isset($_POST['add'])){
  $a3=$_POST['a3'];
  $a4=$_POST['a4'];
 
-$query = "INSERT INTO card(`q1`, `a1`, `a2`, `a3`, `a4`) VALUES ('$q1','$a1','$a2','$a3','$a4')";
+$query = "INSERT INTO card( `q1`, `a1`, `a2`, `a3`, `a4`) VALUES ('$q1','$a1','$a2','$a3','$a4')";
 $reult = mysqli_query($connection,$query);
 if($reult)
 {
@@ -36,30 +36,37 @@ else{
     <title>AddNew</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body class=" container mt-11">
-<div class=" flex justify-center h-screen justify-items-start space-x-6">
-  <div>
-  <div class="rounded-xl  p-10 w-64 shadow-2xl shadow-amber-800">
-    <h1 class=" text-lg text-center  font-serif font-bold mb-3">Create New Card</h1>
+<body class="">
+  <div class="mt-20 flex justify-center space-x-9">
+    <div class="h-96  w-64 rounded-xl p-4 shadow-2xl  duration-1000 hover:scale-105 shadow-red-800">
+      <h1 class="mb-3 text-center font-serif text-lg font-bold">အသစ်တည်ဆောက်ရန်</h1>
 
-   <form action="insert.php" method="post" enctype="multipart/form-data">
-   
-    <?php  inputFild("Question1","q1","textaera"); ?>
-    <?php  inputFild("Answer1","a1","textaera"); ?>
-    <?php  inputFild("Answer2","a2","textaera"); ?>
-    <?php  inputFild("Answer3","a3","textaera"); ?>
-    <?php  inputFild("Answer4","a4","textaera"); ?>
-    
-    <button class="  font-serif font-bold bg-red-300  rounded-lg p-5" name="add">Add</button>
+      <form action="insert.php" method="post" enctype="multipart/form-data">
+        <?php  inputFild("Question1","q1","textaera"); ?>
+        <?php  inputFild("Answer1","a1","textaera"); ?>
+        <?php  inputFild("Answer2","a2","textaera"); ?>
+        <?php  inputFild("Answer3","a3","textaera"); ?>
+        <?php  inputFild("Answer4","a4","textaera"); ?>
 
-    </form>
+        <button class="rounded-md bg-pink-300 p-2 px-3 font-serif text-xs font-bold duration-1000 hover:scale-105" name="add">ထည့်ရန်</button>
+      </form>
+    </div>
+    <div class="flex h-96 w-64 justify-center">
+      <div class="w-64 rounded-xl p-4 shadow-lg shadow-purple-950 duration-1000 hover:scale-105">
+        <h1 class="mb-3 text-center font-serif font-bold">ပြင်ဆင်ရန်</h1></h1>
+        <textarea name="q1" type="text" class="rounded-lg bg-purple-200 px-8"></textarea>
+        <textarea name="q1" type="text" class="rounded-lg bg-purple-200 px-8"></textarea>
+        <textarea name="q1" type="text" class="rounded-lg bg-purple-200 px-8"></textarea>
+        <textarea name="q1" type="text" class="rounded-lg bg-purple-200 px-8"></textarea>
+        <textarea name="q1" type="text" class="mb-2 rounded-lg bg-purple-200 px-8"></textarea>
+        <button class="rounded-md bg-purple-300 p-2 px-3 font-serif text-xs font-bold duration-1000 hover:scale-105">ထည့်ရန်</button>
+      </div>
+    </div>
 
-
+    <!--add-->
   </div>
-  </div><!--add-->
-
-</div>
 </body>
+
 </html>
 
 
