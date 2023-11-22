1ba87ffdc589aaf5f13b3952a21673a5113f2674
Wyine <148890732+Stellaro1@users.noreply.github.com>
2023-11-22 23:11:51 +0630

    Add files via upload

---
+++ operation.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+<?php
+function inputFild($placeholder,$name,$type){
+    $fun = "<textarea class=' w-full  px-1 bg-red-200 rounded-lg hover:scale-105 duration-1000' placeholder='$placeholder' name='$name',type='$type'></textarea>";
+    echo "$fun";
+
+}
+
+?>
